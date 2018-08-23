8f2df489dd035bb46d23c7da0c2e71872328000f
huangtao <huangtao117@gmail.com>
2018-08-23 08:47:11 +0800

    improve ui

--- pages/main/main.js
+++ pages/main/main.js
@@ -4,41 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    cash: 0,
+    cash: 0.00,
     superid: '',
-    motto: '正在登录...',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    routers: [
+      {
+        name: '推广码',
+        url: '/pages/tgm/tgm',
+        icon: 'iconfont icon-qrcode'
+      },
+      {
+        name: '客户',
+        url: '/pages/customer/customer',
+        icon: 'iconfont icon-nav_promoter'
+      },
+      {
+        name: '游戏充值',
+        url: '',
+        icon: 'iconfont icon-48'
+      },
+      {
+        name: '提现记录',
+        url: '',
+        icon: 'iconfont icon-jizhangben'
+      }
+    ]
   },
   //事件处理函数
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
   onButtonCode: function (e) {
     wx.navigateTo({
@@ -55,13 +47,5 @@ Page({
   },
   onButtonTX: function (e) {
 
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
