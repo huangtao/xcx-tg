b61baf11dc8c84b1b1e2a150ee648dcf46f827e6
huangtao117 <huangtao117@gmail.com>
2018-09-16 16:38:42 +0800

    i

--- pages/main/main.js
+++ pages/main/main.js
@@ -31,6 +31,9 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
+    this.setData({
+      cash: app.globalData.userInfo.cash
+    })
   },
   onButtonCode: function (e) {
     wx.navigateTo({
@@ -46,6 +49,46 @@ Page({
 
   },
   onButtonTX: function (e) {
-
+    // if (this.data.cash < 10) {
+    //   // 10元以下不能提现
+    //   wx.showToast({
+    //     title: '超过10元才能提现!',
+    //     icon: 'none',
+    //     duration: 3000
+    //   })
+    //   return
+    // }
+    // 发送提现请求
+    var cookie = wx.getStorageSync('cookieKey');
+    var header = {};
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    wx.request({
+      url: 'https://www.yunpai8.cn/ldyx/xcx/ldyxTg/reqtx.php',
+      header: header,
+      data: {
+        cash: this.data.cash
+      },
+      success: res => {
+        console.log(res.data);
+        if (res.data.code == 0) {
+          wx.showToast({
+            title: '申请提现成功，审核中...',
+            icon: 'none',
+            duration: 3000
+          })
+          this.setData({
+            cash: 0
+          });
+        } else {
+          wx.showToast({
+            title: '申请提现失败!',
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      }
+    })
   }
 })
